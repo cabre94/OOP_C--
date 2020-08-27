9f12c2017951d287cdeac7150e7a186c3d277db8
Cabre <cabre94@hotmail.com>
2020-08-26 23:10:50 -0300

    Agrego ejercicio 2

---
+++ Practica_2/ej_2.cpp
@@ -0,0 +1,30 @@
+/*
+date: 26-08-2020
+File: ej_2.cpp
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description:
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <sstream>
+#include <cstdlib>
+
+class prueba{
+    int fun(){
+        return 0;
+    }
+};
+
+// Compilar usando g++ -c namefile.cpp
+// Compilar usando gcc -c namefile.c
+// Despues abrir los dos .o con nm filename.o o si no CREO objdump
