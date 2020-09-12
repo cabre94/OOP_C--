dbac6d4ecde35cefedc8aafc1566b03cac43030d
Cabre <cabre94@hotmail.com>
2020-09-12 07:23:53 -0300

    Initial commit

---
+++ Practica_5/ej_6.cpp
@@ -0,0 +1,21 @@
+/*
+date: 12-09-2020
+File: ej_6.cpp
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description:
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <list>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <sstream>
+#include <cstdlib>
+
