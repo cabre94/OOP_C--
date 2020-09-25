963c0ee9a5c6603ff0d85d373700a2e7c71dbbd3
Cabre <cabre94@hotmail.com>
2020-09-25 18:07:02 -0300

    Implementacion del Stack para el ejercicio 8

---
+++ Practica_7/stack.h
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <array>
+#include <vector>
+#include <algorithm>
+
+// Implementacion de Stack del ejercicio 3 de la practica 2
+
+template <class T>
+class Stack{
+    T *v;
+    int max_size, top;
+public:
+    class Underflow {};
+    class Overflow {};
+    class Bad_size {};
+    
+    Stack(int size);
+    ~Stack();
+
+    void push(T);
+    T pop();
+    bool isEmpty();
+
+    // Sobrecargo <<. Es posible que para tipos de datos 'raros' falle.
+    template<class H>
+    friend std::ostream& operator<<(std::ostream &out, Stack<H> &s);
+};
+
+#define STACK_MAX_SIZE 10000
+
+template<class T>
+Stack<T>::Stack(int size){
+    if(size > STACK_MAX_SIZE)
+        throw Bad_size{};
+    max_size = size;
+    top = 0;
+    v = new T[size];
+}
+
+template<class T>
+Stack<T>::~Stack(){
+    delete [] v;
+}
+
+template<class T>
+void Stack<T>::push(T t){
+    if(top == max_size)
+        throw Overflow{};
+    v[top++] = t;
+}
+
+template<class T>
+T Stack<T>::pop(){
+    if(top == 0)
+        throw Underflow{};
+    return v[--top];
+}
+
+template<class T>
+bool Stack<T>::isEmpty(){
+    return top == 0;
+}
+
+template<class U>
+//std::ostream& operator<<(ostream &out, Stack &s);
+std::ostream& operator<<(std::ostream &out, Stack<U> &s){
+    if(s.top == 0){
+        out << "Stack is Empty";
+    }else{
+        int i;
+        for(i=0; i < s.top-1; i++)
+            out << s.v[i] << " -> ";
+        out << s.v[i];
+    }
+    return out;
+}
